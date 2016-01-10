02df38449297ad6c305bc01f28f6010431afb18a
Ankit <ankit.bharat.tanna@accenture.com>
2016-01-10 13:41:55 +0530

    SMC: Initial Commit
    SMC: Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the smcApp
+ */
+angular.module('smcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
